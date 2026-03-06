ca8486d03edfbe7edc3ad78452809d9b96befff0
KShakilRana2026 <jiyakhan3khanhh@gmail.com>
2026-03-06 20:35:52 +0600

    Add configuration for bot token and channels

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+FORCE_CHANNELS = [
+    "@dark_princes12",      # Group
+    "@myfirstchannel12"     # Channel
+]
+
+BASE_URL = "https://tapmail-prime.vercel.app/api"
